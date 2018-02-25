f841faf777a67d8180a0060f041d67318aa3f669
Jovascript <joe.bell34@btinternet.com>
2018-02-25 19:17:38 +0000

    Tweaks2

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from drawpi.hardware.plotter import Plotter
 from drawpi.point import Point
+import logging
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
 
 x = Plotter()
-x.goto(Point(8*200, 8*200, unit="steps"))
+x.goto(Point(8*200*3, 8*200*5, unit="steps"))
+print("NOOT")
 
